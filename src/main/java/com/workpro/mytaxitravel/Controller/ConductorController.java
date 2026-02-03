a4b007ddb17d4463496682df1bfce72d388635f7
S109shiro <sebgonrod@gmail.com>
2026-02-02 22:36:23 -0500

    Cambio en endpoints e implementacion del controller Conductor

---
+++ src/main/java/com/workpro/mytaxitravel/Controller/ConductorController.java
@@ -0,0 +1,46 @@
+package com.workpro.mytaxitravel.Controller;
+
+import com.workpro.mytaxitravel.Entity.Conductor;
+import com.workpro.mytaxitravel.Service.ConductorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/conductor")
+public class ConductorController {
+    // Instancia de service del conductor
+    @Autowired
+    ConductorService conductorService;
+
+    @GetMapping("/getAll")
+    public List<Conductor> getConductores(){
+        return conductorService.getConductores();
+    }
+
+    @GetMapping("/get/{id_conductor}")
+    public Optional<Conductor> getConductor(@PathVariable("id_conductor") int idConductor){
+        return conductorService.getConductor(idConductor);
+    }
+
+    @PostMapping("/create")
+    public String saveConductor(@RequestBody Conductor conductor){
+        conductorService.saveConductor(conductor);
+        return "Conductor registrado con el siguiente id: " + conductor.getIdConductor();
+    }
+
+    @PutMapping("/update")
+    public String updateConductor(@RequestBody Conductor conductor){
+        conductorService.updateConductor(conductor);
+        return "El conductor con el id: " + conductor.getIdConductor() + " ha sido modificado.";
+    }
+
+    @DeleteMapping("/delete/{id_conductor}")
+    public String deleteConductor(@PathVariable("id_conductor") int idConductor){
+        conductorService.deleteConductor(idConductor);
+        return "El conductor con el id: " + idConductor + " ha sido eliminado.";
+    }
+
+}
--- src/main/java/com/workpro/mytaxitravel/Controller/UsuarioController.java
+++ src/main/java/com/workpro/mytaxitravel/Controller/UsuarioController.java
@@ -1,48 +1,46 @@
 package com.workpro.mytaxitravel.Controller;
 
 
-import com.workpro.mytaxitravel.Entity.Persona;
 import com.workpro.mytaxitravel.Entity.Usuario;
 import com.workpro.mytaxitravel.Service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 import java.util.Optional;
 
 @RestController  // Indica que esta clase es un rest controller el cual permite
 // Manejar solicitudes http - Devuelve objetos como Json o xml - Es un endpoint
-@RequestMapping(path = "/usuarios")  // Indica la url del controlador a utilizar
+@RequestMapping(path = "/usuario")  // Indica la url del controlador a utilizar
 public class UsuarioController {
     @Autowired  //
     private UsuarioService usuarioService;  // Traemos los servicios para aplicar los metodos de la logica de negocio
 
-    @GetMapping(path = "/getUsuarios")
+    @GetMapping(path = "/getAll")
     public List<Usuario> getUsuarios(){
         return usuarioService.getUsuarios();  // LLamamos al service
     }
 
-    @GetMapping(path = "/getUsuario/{id_usuario}")
+    @GetMapping(path = "/get/{id_usuario}")
     public Optional<Usuario> getUsuario(@PathVariable("id_usuario") int idUsuario){  // Path es para obtener una variable de la url
         return usuarioService.getUsuario(idUsuario);
     }
 
-    @PostMapping(path = "/createUsuario")
+    @PostMapping(path = "/create")
     public String createUsuario(@RequestBody Usuario usuario){
         usuarioService.saveUsuario(usuario);
-        return "Usuario registrado con el Id: " + usuario.getIdUsuario();
+        return "Usuario registrado con el siguiente id: " + usuario.getIdUsuario();
     }
 
-    @PutMapping(path = "/updateUsuario")
+    @PutMapping(path = "/update")
     public String updateUsuario(@RequestBody Usuario usuario){
         usuarioService.updateUsuario(usuario);
-        return "El usuario: " + usuario.getNombre() + " ha sido actualizado";
+        return "El usuario con el id: " + usuario.getNombre() + " ha sido actualizado";
     }
 
-    @DeleteMapping(path = "/deleteUsuario/{id_usuario}")
-    public String deleteUsuario(@PathVariable("id_usuario") int id){
-        usuarioService.deleteUsuario(id);
-        return "El usuario con el id: " + id + " ha sido eliminado";
+    @DeleteMapping(path = "/delete/{id_usuario}")
+    public String deleteUsuario(@PathVariable("id_usuario") int idUsuario){
+        usuarioService.deleteUsuario(idUsuario);
+        return "El usuario con el id: " + idUsuario + " ha sido eliminado";
     }
 }
